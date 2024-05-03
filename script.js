ec97d4db1aa5f0d66b3ca0f4067a5b3516843a1c
Iuliia <iuliiaobr.io@gmail.com>
2024-05-03 16:45:35 +0700

    change circle size based in range input

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const btnElement = document.querySelector("#btn");
-const textInputElement = document.querySelector("input[type=text] ");
+const textInputElement = document.querySelector("input[type=text]");
+const rangeInputElement = document.querySelector("input[type=range]");
 const squareElement = document.querySelector("#square");
+const circleElement = document.querySelector("#circle");
+const circleBtnElement = document.querySelector("#circle > #e_btn");
+
+circleBtnElement.style.display = "none";
 
 const changeSquareColor = function () {
-  btnElement.addEventListener("click", () => {
-    const value = textInputElement.value;
+  btnElement.addEventListener("click", (e) => {
+    const value = e.target.value;
 
     squareElement.style.backgroundColor = value;
-    console.log(value);
+  });
+};
+
+const changeCircleSize = function () {
+  rangeInputElement.addEventListener("input", (e) => {
+    const value = e.target.value;
+
+    circleElement.style.width = `${value}%`;
+    circleElement.style.height = `${value}%`;
   });
 };
 
 changeSquareColor();
+changeCircleSize();
