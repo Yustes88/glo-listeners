90cc72f2871592b10cae7027ffd1a98be722f7d2
Iuliia <iuliiaobr.io@gmail.com>
2024-05-03 16:34:54 +0700

    change square background color on button click with input

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+const btnElement = document.querySelector("#btn");
+const textInputElement = document.querySelector("input[type=text] ");
+const squareElement = document.querySelector("#square");
 
+const changeSquareColor = function () {
+  btnElement.addEventListener("click", () => {
+    const value = textInputElement.value;
 
+    squareElement.style.backgroundColor = value;
+    console.log(value);
+  });
+};
 
-
-
+changeSquareColor();
