231cb74b08cc8d31b2bd787fb3f9a60d9663e91b
Iuliia <iuliiaobr.io@gmail.com>
2024-05-07 16:37:40 +0700

    fix bugs

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btnElement = document.querySelector("#btn");
 const textInputElement = document.querySelector("input[type=text]");
 const rangeInputElement = document.querySelector("input[type=range]");
+const rangeSpanElement = document.querySelector("#range-span");
 const squareElement = document.querySelector("#square");
 const circleElement = document.querySelector("#circle");
 const circleBtnElement = document.querySelector("#circle > #e_btn");
@@ -8,16 +9,18 @@ const circleBtnElement = document.querySelector("#circle > #e_btn");
 circleBtnElement.style.display = "none";
 
 const changeSquareColor = function () {
-  btnElement.addEventListener("click", (e) => {
-    const value = e.target.value;
+  btnElement.addEventListener("click", () => {
+    const value = textInputElement.value;
 
     squareElement.style.backgroundColor = value;
   });
 };
 
 const changeCircleSize = function () {
-  rangeInputElement.addEventListener("input", (e) => {
-    const value = e.target.value;
+  rangeInputElement.addEventListener("input", () => {
+    const value = rangeInputElement.value;
+
+    rangeSpanElement.textContent = value;
 
     circleElement.style.width = `${value}%`;
     circleElement.style.height = `${value}%`;
